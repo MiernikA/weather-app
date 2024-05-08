f139031a06d8d51e50c1270b62872997cfd41bac
Adrian Miernik <108336626+MiernikA@users.noreply.github.com>
2024-05-08 15:32:23 +0200

    Update ComponentStyles.js

--- src/constants/ComponentStyles.js
+++ src/constants/ComponentStyles.js
@@ -9,7 +9,7 @@ const StyledCard = styled(Card, {
     margin: '1.02vw',
     justifyContent: 'space-between',
     background: mode ? 'radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)' : 'radial-gradient(circle at 18.7% 37.8%, rgb(40, 40, 40) 0%, rgb(25, 30, 35) 90%)',
-    boxShadow: mode ? '0 2px 8px rgba(0, 0, 0, 0.4)' : '0 2px 8px rgba(255, 255, 255, 0.4)',
+    boxShadow: mode ? '0 2px 8px rgba(0, 0, 0, 0.4)' : '0 2px 8px rgba(248, 248, 248, 0.4)',
     userSelect: 'none',
     color: mode ? 'rgb(0,0,0)' : 'rgb(233,233,233)',
     '@media (max-width: 1300px)': {
